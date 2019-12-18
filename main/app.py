4fcd1003a9c4d67013510fada9714e8f2ae72bdc
marianoiglesiasmarchese <marianoiglesiasmarchese@gmail.com>
2019-12-17 22:10:24 -0300

    feature(): smart_open example

--- main/app.py
+++ main/app.py
@@ -3,6 +3,7 @@ import os
 import logging
 import pandas as pd
 import boto3
+from smart_open import open
 
 
 # setup logger
@@ -11,10 +12,20 @@ logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO').upper())
 
 
 def lambda_handler(event, context):
+    # shared attributes
+    s3_endpoint = os.environ.get('S3_ENDPOINT', 'http://s3.amazonaws.com/')
+    s3_bucket = event.get("bucket")
+    s3_key = event.get("key")
+
     """
-    stream reading and writing
+    stream reading and writing with boto3
     """
-    read_and_persistence_with_boto3(event)
+    read_and_persistence_with_boto3(s3_endpoint, s3_bucket, s3_key)
+
+    """
+    stream reading and writing with smart_open
+    """
+    read_and_persistence_with_smart_open(s3_endpoint, s3_bucket, s3_key)
 
     return {
         "statusCode": 200,
@@ -24,11 +35,8 @@ def lambda_handler(event, context):
     }
 
 
-def read_and_persistence_with_boto3(event):
+def read_and_persistence_with_boto3(s3_endpoint, s3_bucket, s3_key):
     # boto3 s3 client
-    s3_endpoint = os.environ.get('S3_ENDPOINT', 'http://s3.amazonaws.com/')
-    s3_bucket = event.get("bucket")
-    s3_key = event.get("key")
     s3_client = boto3.client('s3', endpoint_url=s3_endpoint)
 
     # fetching an object
@@ -39,3 +47,23 @@ def read_and_persistence_with_boto3(event):
     # pushing it!
     s3_client.put_object(Body=csv_file_df.to_csv(index=False), Bucket=s3_bucket, Key='result/' + s3_key)
     logger.info("persistence on aws s3 done with boto3!")
+
+
+def read_and_persistence_with_smart_open(s3_endpoint, s3_bucket, s3_key):
+    # Stream from/to localstack bucket providing endpoint from boto3
+    transport_params = {
+        'session': boto3.Session(),
+        'resource_kwargs': {
+            'endpoint_url': s3_endpoint
+        }
+    }
+
+    # fetching and object
+    with open('s3://' + s3_bucket + '/' + s3_key, transport_params=transport_params) as file:
+        csv_file_df = pd.read_csv(file)
+        logger.info('file: \n %s', csv_file_df)
+
+    # pushing it!
+    with open('s3://test-bucket/key.txt', 'w', transport_params=transport_params) as fout:
+        csv_file_df.to_csv(fout, index=False)
+        logger.info("persistence on aws s3 done with smart_open!")
